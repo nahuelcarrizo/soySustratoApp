c496fab03455e1d4cbe9261695a5d9431875fd26
Nahuel Carrizo <nhcarrizo@gmail.com>
2022-05-28 10:30:54 -0300

    creación navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import logo from './soysustrato-logoblanco.png';
+
+
+function NavBar() {
+    return  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+              <a className="navbar-brand ms-4" href="#">
+                <img src={logo} width="110" height="35" alt="logo de soy sustrato" />
+              </a>
+              <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mx-auto">
+                  <li className="nav-item">
+                    <a className="nav-link">Productos</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link">Nosotros</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link">FAQS</a>
+                  </li>
+                </ul>
+              </div>
+              <form class="form-inline me-4">
+                <button class="btn btn-light" type="button">Login</button>
+              </form>
+            </nav>
+  }
+  
+  export default NavBar;
