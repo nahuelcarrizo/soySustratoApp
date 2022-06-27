866b95723ccab46682ff5e196a0ac0a1e96d09f2
Nahuel Carrizo <nhcarrizo@gmail.com>
2022-06-26 21:35:00 -0300

    spinner

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import Item from "./Item";
 
 export default function ItemList({ productos }) {
+  const [isLoading, setIsLoading] = useState(true);
   const [isOk, setIsOk] = useState([]);
 
   console.log("se renderiza ItemList");
 
   useEffect(() => {
     const respOk = new Promise((res, rej) => {
-      res(productos);
+      setTimeout(() => {
+        res(productos);
+      }, 2600);
     });
 
     respOk
@@ -17,16 +20,32 @@ export default function ItemList({ productos }) {
       })
       .catch(() => {
         console.log("Algo falló");
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [productos]);
 
   return (
     <div className="row justify-content-center">
-      <div className="mb-5 mt-5 text-center">
-        <h1>Tienda.</h1>
-        <h2>CONOCE NUESTROS PRODUCTOS QUE SE HACEN TIERRA EN 180 DÍAS.</h2>
-      </div>
-
+      {!isLoading && (
+        <div className="mb-5 mt-5 text-center">
+          <h1>Tienda.</h1>
+          <h2>CONOCE NUESTROS PRODUCTOS QUE SE HACEN TIERRA EN 180 DÍAS.</h2>
+        </div>
+      )}
+      {isLoading && (
+        <div className="d-flex flex-column align-items-center mt-5">
+          <div
+            className="spinner-border"
+            style={{ width: "3rem", height: "3rem" }}
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <p>Cargando todos los productos</p>
+        </div>
+      )}
       {isOk && isOk.map((item) => <Item key={item.id} item={item} />)}
     </div>
   );
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -7,13 +7,12 @@ export default function ItemListContainer() {
   let prods = productosData;
   const { categoryId } = useParams();
   const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getProductsByCategory = new Promise((res, rej) => {
       setTimeout(() => {
         res(prods.filter((el) => el.categoria === categoryId));
-      }, 2200);
+      }, 200);
     });
 
     getProductsByCategory
@@ -22,30 +21,14 @@ export default function ItemListContainer() {
       })
       .catch(() => {
         console.log("falló");
-      })
-      .finally(() => {
-        setIsLoading(false);
       });
   }, [categoryId]);
 
-  console.log(products.length == 0);
   return (
     <>
       <section>
         <div className="container">
-          {isLoading && (
-            <div className="d-flex flex-column align-items-center mt-5">
-              <div
-                className="spinner-border"
-                style={{ width: "3rem", height: "3rem" }}
-                role="status"
-              >
-                <span className="visually-hidden">Loading...</span>
-              </div>
-              <p>Cargando todos los productos</p>
-            </div>
-          )}
-          {products.length == 0 ? (
+          {categoryId !== undefined ? (
             <ItemList productos={products} />
           ) : (
             <ItemList productos={prods} />
