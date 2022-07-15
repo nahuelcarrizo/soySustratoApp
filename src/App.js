c03794d2367ebb93accf02471b6ab43e533847b0
Nahuel Carrizo <nhcarrizo@gmail.com>
2022-07-15 17:03:46 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 //@ts-check
-import Navbar from "./components/Navbar";
-import './App.css';
+import Navbar from "./components/NavBar";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Tienda from './components/Tienda';
+import Tienda from "./components/Tienda";
 import Shop from "./components/Shop/Shop";
 import Home from "./components/Home";
 
-
-
 export default function App() {
-  return (<>
-  <BrowserRouter>
-  <Navbar />
-    <Routes>
-    <Route path="/home" element={<Home />} />
-      <Route path="/tienda" element={<Tienda />} />
-      <Route path="/shop/shop" element={<Shop />} />
-
-    </Routes>
-  </ BrowserRouter>
-  </>
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/tienda" element={<Tienda />} />
+          <Route path="/shop/shop" element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
